428ebc45d4be74dd31842f954fe10c11dca48917
mohamedmax728 <40329482+mohamedmax728@users.noreply.github.com>
2022-05-02 03:46:21 +0200

    first commit

---
+++ archi_Template/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace archi_Template
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.runCyBtn = new System.Windows.Forms.Button();
+            this.StartPCTxt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.inialBtn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.MemGrid = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PiplGrid = new System.Windows.Forms.DataGridView();
+            this.valueCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.registerCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MipsRegGrid = new System.Windows.Forms.DataGridView();
+            this.UserCodetxt = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MemGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PiplGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MipsRegGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // runCyBtn
+            // 
+            this.runCyBtn.Location = new System.Drawing.Point(622, 534);
+            this.runCyBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.runCyBtn.Name = "runCyBtn";
+            this.runCyBtn.Size = new System.Drawing.Size(169, 70);
+            this.runCyBtn.TabIndex = 23;
+            this.runCyBtn.Text = "Run 1 cycle";
+            this.runCyBtn.UseVisualStyleBackColor = true;
+            this.runCyBtn.Click += new System.EventHandler(this.runCycleBtn_Click);
+            // 
+            // StartPCTxt
+            // 
+            this.StartPCTxt.Location = new System.Drawing.Point(57, 554);
+            this.StartPCTxt.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.StartPCTxt.Name = "StartPCTxt";
+            this.StartPCTxt.Size = new System.Drawing.Size(116, 24);
+            this.StartPCTxt.TabIndex = 21;
+            this.StartPCTxt.Text = "1000";
+            this.StartPCTxt.TextChanged += new System.EventHandler(this.StartPCTxt_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(20, 554);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 20);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "PC:";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "Value";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "Address";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            // 
+            // inialBtn
+            // 
+            this.inialBtn.Location = new System.Drawing.Point(290, 534);
+            this.inialBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.inialBtn.Name = "inialBtn";
+            this.inialBtn.Size = new System.Drawing.Size(169, 70);
+            this.inialBtn.TabIndex = 22;
+            this.inialBtn.Text = "Inisialize";
+            this.inialBtn.UseVisualStyleBackColor = true;
+            this.inialBtn.Click += new System.EventHandler(this.inializeBtn_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1069, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 20);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Data memory";
+            // 
+            // MemGrid
+            // 
+            this.MemGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MemGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.MemGrid.Location = new System.Drawing.Point(1072, 68);
+            this.MemGrid.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MemGrid.Name = "MemGrid";
+            this.MemGrid.ReadOnly = true;
+            this.MemGrid.Size = new System.Drawing.Size(281, 459);
+            this.MemGrid.TabIndex = 18;
+            this.MemGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.MemoryGrid_CellContentClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(750, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Pipline registers";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(419, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "MIPS registers";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Value";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Register";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // PiplGrid
+            // 
+            this.PiplGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.PiplGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.PiplGrid.Location = new System.Drawing.Point(754, 68);
+            this.PiplGrid.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.PiplGrid.Name = "PiplGrid";
+            this.PiplGrid.ReadOnly = true;
+            this.PiplGrid.Size = new System.Drawing.Size(281, 459);
+            this.PiplGrid.TabIndex = 14;
+            // 
+            // valueCol
+            // 
+            this.valueCol.HeaderText = "Value";
+            this.valueCol.Name = "valueCol";
+            this.valueCol.ReadOnly = true;
+            // 
+            // registerCol
+            // 
+            this.registerCol.HeaderText = "Register";
+            this.registerCol.Name = "registerCol";
+            this.registerCol.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "user code";
+            // 
+            // MipsRegGrid
+            // 
+            this.MipsRegGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MipsRegGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.registerCol,
+            this.valueCol});
+            this.MipsRegGrid.Location = new System.Drawing.Point(422, 68);
+            this.MipsRegGrid.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MipsRegGrid.Name = "MipsRegGrid";
+            this.MipsRegGrid.ReadOnly = true;
+            this.MipsRegGrid.Size = new System.Drawing.Size(282, 459);
+            this.MipsRegGrid.TabIndex = 13;
+            this.MipsRegGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.MipsRegisterGrid_CellContentClick);
+            // 
+            // UserCodetxt
+            // 
+            this.UserCodetxt.Location = new System.Drawing.Point(19, 68);
+            this.UserCodetxt.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.UserCodetxt.Multiline = true;
+            this.UserCodetxt.Name = "UserCodetxt";
+            this.UserCodetxt.Size = new System.Drawing.Size(370, 458);
+            this.UserCodetxt.TabIndex = 12;
+            this.UserCodetxt.TextChanged += new System.EventHandler(this.UserCodetxt_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1365, 646);
+            this.Controls.Add(this.runCyBtn);
+            this.Controls.Add(this.StartPCTxt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.inialBtn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.MemGrid);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PiplGrid);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MipsRegGrid);
+            this.Controls.Add(this.UserCodetxt);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.MemGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PiplGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MipsRegGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button runCyBtn;
+        private System.Windows.Forms.TextBox StartPCTxt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.Button inialBtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView MemGrid;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridView PiplGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn valueCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn registerCol;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView MipsRegGrid;
+        private System.Windows.Forms.TextBox UserCodetxt;
+    }
+}
+
---
+++ archi_Template/Form1.cs
@@ -0,0 +1,434 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace archi_Template
+{
+    public partial class Form1 : Form
+    {
+        public int pc = 1000;
+        public int pop = 0;
+        public Hashtable Inst_mem = new Hashtable();
+        public Hashtable dt_mem = new Hashtable();
+        public int[] reg = new int[32];
+
+        public Queue dcodequeue = new Queue();
+        public Queue exqueue = new Queue();
+        public Queue mmqueue = new Queue();
+        public Queue wbqueue = new Queue();
+        public Int64 wb;
+        public int register1, register2;
+        public string contit(string op)
+        {
+            string regds = "", alop1 = "", alop2 = "", ALUsrc = "", meread = "", mewrite = "", branch = "", regwri = "", metoreg = "";
+            string e, me, w;
+            if (op == "000000")
+            {
+                regds = "1";
+                alop1 = "1";
+                alop2 = "0";
+                ALUsrc = "0";
+                meread = "0";
+                mewrite = "0";
+                branch = "0";
+                regwri = "1";
+                metoreg = "0";
+            }
+            else if (op == "100011")
+            {
+                regds = "0";
+                alop1 = "0";
+                alop2 = "0";
+                ALUsrc = "1";
+                meread = "1";
+                mewrite = "0";
+                branch = "0";
+                regwri = "1";
+                metoreg = "1";
+            }
+            e = regds + alop1 + alop2 + ALUsrc;
+            me = meread + mewrite + branch;
+            w = regwri + metoreg;
+            return e + me + w;
+        }
+        public void instructionmemory(string pccor, string inst)
+        {
+            Inst_mem.Add(pccor, inst);
+        }
+        public void register_file(int redr1, int redr2, int writere, int wrdate, int rwrite)
+        {
+
+            if (rwrite == 0)
+            {
+                register1 = reg[redr1];
+                register2 = reg[redr2];
+            }
+            else
+            {
+                reg[writere] = wrdate;
+            }
+        }
+        public Hashtable dame = new Hashtable();
+
+        public Int64 aluoregis(Int64 d1, Int64 d2, string al, string funcode)
+        {
+            if (al == "00")
+                return d1 + d2;
+            else
+            {
+                if (funcode == "100000")
+                {
+                    return d1 + d2;
+                }
+                else if (funcode == "100010")
+                {
+                    return d1 - d2;
+                }
+                else if (funcode == "100100")
+                {
+                    return d1 & d2;
+                }
+                else
+                {
+                    return d1 | d2;
+                }
+            }
+
+        }
+        public object data_memory(int address, int write_d, int m_write, int m_read)
+        {
+            if (m_read == 1)
+            {
+                return dame[address];
+            }
+            if (m_write == 1)
+            {
+                dame[address] = write_d;
+            }
+            return null;
+        }
+        public Int64 mu(Int64 s1, Int64 s2, int select)
+        {
+            if (select == 0)
+                return s1;
+            else return s2;
+        }
+        public Int64 signExt(Int64 adddddd)
+        {
+            Int64 value = (0x0000FFFF & adddddd);
+            Int64 mak = 0x00008000;
+            Int64 signsda = (mak & adddddd) >> 15;
+            if (signsda == 1)
+                value += 0xFFFF0000;
+            return value;
+        }
+
+        public void fetch()
+        {
+            string If_id;
+            pc += 4;
+            If_id = pc.ToString() + ":" + Inst_mem[(pc - 4).ToString()];
+            dcodequeue.Enqueue(If_id.ToString());
+
+
+        }
+
+        public void decode()
+        {
+
+            string If_id = (string)dcodequeue.Dequeue();
+            string[] part = If_id.Split(':');
+            string opp = part[1].Substring(0, 6);
+            string rs = "", rt = "", rd = "", shamt = "", fun = "", address = "";
+            string cont = contit(opp);
+            Int64 extended = 0;
+            if (opp == "000000")
+            {
+                rs = part[1].Substring(6, 5);
+                rt = part[1].Substring(11, 5);
+                rd = part[1].Substring(16, 5);
+                shamt = part[1].Substring(21, 5);
+                fun = part[1].Substring(26, 6);
+
+                register_file(Convert.ToInt32(rs, 2), Convert.ToInt32(rt, 2), 0, 0, 0);
+                extended = signExt(Int64.Parse(rd + shamt + fun));
+            }
+            else if (opp == "100011")
+            {
+                rs = part[1].Substring(6, 5);
+                rt = part[1].Substring(11, 5);
+                address = part[1].Substring(16, 16);
+                rd = part[1].Substring(16, 5);
+                register_file(Convert.ToInt32(rs, 2), Convert.ToInt32(rt, 2), 0, 0, 0);
+                extended = signExt(Int64.Parse(address));
+            }
+            string id_ex;
+            id_ex = cont + ':' + part[0] + ':' + register1 + ':' + register2 + ':' + extended + ':' + rd + ':' + rt + ':' + part[1].Substring(26, 6);
+            exqueue.Enqueue(id_ex);
+        }
+
+        public void exc()
+        {
+            string id_ex = (string)exqueue.Dequeue();
+            string[] part = id_ex.Split(':');
+            Int64 mualsource, mureg = 0;
+
+            Int64 au = 0;
+            string rt = part[6];
+            string rd = part[5];
+
+            if (part[0][0] == '1')
+            {
+                mualsource = mu(Int64.Parse(part[4]), Int64.Parse(part[3]), part[0][3]);
+                string fun = part[7];
+                au = aluoregis(Int64.Parse(part[2]), mualsource, "10", fun);
+                mureg = mu(Convert.ToInt32(rt, 2), Convert.ToInt32(rd, 2), part[0][0]);
+
+            }
+            else if (part[0][0] == '0')
+            {
+                mualsource = mu(Int64.Parse(part[4]), Int64.Parse(part[3]), part[0][3]);
+                au = aluoregis(register1, mualsource, "00", "0");
+                mureg = mu(Convert.ToInt32(rt, 2), Convert.ToInt32(rd, 2), part[0][0]);
+            }
+            string ex_mem;
+            ex_mem = part[0].Substring(4, 5) + ':' + au.ToString() + ':' + part[3] + ':' + mureg.ToString();
+            mmqueue.Enqueue(ex_mem);
+
+        }
+
+        public void memorandwb()
+        {
+
+            string ex_mem = (string)mmqueue.Dequeue();
+            string[] part = ex_mem.Split(':');
+            string au = part[1];
+            string mureg = part[3];
+            int tmp = 0;
+            //string opp = part[2];
+
+            if (part[0][0] == '1')
+            {
+                tmp = (int)dt_mem[Int32.Parse(part[1])];
+                // metorg = mu(Int64.Parse(au),99,1);
+            }
+            else if (part[0][1] == '1')
+            {
+                dt_mem[Int32.Parse(part[1])] = part[2];
+                //metorg = mu(Int64.Parse(au), 99, 0);
+            }
+            string m = part[0].Substring(3, 2) + ':' + tmp.ToString() + ':' + part[1] + ':' + part[3];
+            wbqueue.Enqueue(m);
+        }
+
+        public void writek()
+        {
+            string metorg = (string)wbqueue.Dequeue();
+            string[] part = metorg.Split(':');
+            string au = part[2];
+            string mureg = part[3];
+            int muout = 0;
+            if (part[0][1] == '0')
+            {
+                muout = int.Parse(part[2]);
+            }
+            else if (part[0][1] == '1')
+            {
+                muout = int.Parse(part[1]);
+            }
+
+            if (part[0][0] == '1')
+            {
+                reg[int.Parse(part[3])] = muout;
+            }
+
+        }
+        private void UserCodetxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StartPCTxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MipsRegisterGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void inializeBtn_Click(object sender, EventArgs e)
+        {
+            pc = int.Parse(StartPCTxt.Text);
+            string s;
+            for (int i = 0; i < UserCodetxt.Lines.Length; i++)
+            {
+                s = UserCodetxt.Lines[i];
+                string[] part = s.Split(':');
+                instructionmemory(part[0], part[1]);
+                // fetch();
+                // fetchqueue.Enqueue(If_id);
+            }
+            reg[0] = 0;
+            for (int i = 1; i < 32; i++)
+            {
+                reg[i] = 100 + i;
+            }
+            for (int i = 0; i < 256; i++)
+            {
+                dt_mem[i] = 99;
+            }
+            MipsRegGrid.Rows.Add(32);
+            for (int i = 0; i < 32; i++)
+            {
+                MipsRegGrid.Rows[i].Cells[0].Value = "$" + i.ToString();
+                MipsRegGrid.Rows[i].Cells[1].Value = reg[i].ToString();
+
+            }
+            MemGrid.Rows.Add(256);
+
+            for (int i = 0; i < 256; i++)
+            {
+                MemGrid.Rows[i].Cells[0].Value = i.ToString();
+                MemGrid.Rows[i].Cells[1].Value = "99";
+            }
+        }
+
+        private void MemoryGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        public int cy = 1;
+        private void runCycleBtn_Click(object sender, EventArgs e)
+        {
+            do
+            {
+                foreach (DataGridViewRow row in PiplGrid.Rows)
+                {
+                    try
+                    {
+                        PiplGrid.Rows.Remove(row);
+                    }
+                    catch (Exception) { }
+                }
+            } while (PiplGrid.Rows.Count > 1);
+            do
+            {
+                foreach (DataGridViewRow row in MipsRegGrid.Rows)
+                {
+                    try
+                    {
+                        MipsRegGrid.Rows.Remove(row);
+                    }
+                    catch (Exception) { }
+                }
+            } while (MipsRegGrid.Rows.Count > 1);
+            do
+            {
+                foreach (DataGridViewRow row in MemGrid.Rows)
+                {
+                    try
+                    {
+                        MemGrid.Rows.Remove(row);
+                    }
+                    catch (Exception) { }
+                }
+            } while (MemGrid.Rows.Count > 1);
+            if (wbqueue.Count > 0)
+            {
+                writek();
+
+            }
+            if (mmqueue.Count > 0)
+            {
+                memorandwb();
+            }
+            if (exqueue.Count > 0)
+            {
+                exc();
+            }
+            if (dcodequeue.Count > 0)
+            {
+                decode();
+            }
+            if (Inst_mem.ContainsKey(pc.ToString()))
+            {
+                fetch();
+            }
+            show();
+        }
+        void show()
+        {
+
+            //this.PiplGrid.Rows.Add("decode", "excute", "memory access", "write back");
+            // this.PiplGrid.Rows.Insert(0, 0, 0, 0);
+            if (wbqueue.Count > 0)
+            {
+                this.PiplGrid.Rows.Add("write back", wbqueue.Peek());
+                // PiplGrid.Rows[3].Cells[1].Value = ;
+                // PiplGrid.Rows[3].Cells[0].Value = "write back";
+            }
+
+            if (mmqueue.Count > 0)
+            {
+                this.PiplGrid.Rows.Add("memory access", mmqueue.Peek());
+                //  PiplGrid.Rows[2].Cells[0].Value = "memory access";
+                // PiplGrid.Rows[2].Cells[1].Value =;
+            }
+            if (exqueue.Count > 0)
+            {
+                this.PiplGrid.Rows.Add("excute", exqueue.Peek());
+                // PiplGrid.Rows[1].Cells[0].Value = "excute";
+
+            }
+            if (dcodequeue.Count > 0)
+            {
+                this.PiplGrid.Rows.Add("decode", dcodequeue.Peek());
+                //PiplGrid.Rows[0].Cells[0].Value = "decode";
+
+            }
+
+
+
+
+
+            MipsRegGrid.Rows.Add(32);
+            for (int i = 0; i < 32; i++)
+            {
+                MipsRegGrid.Rows[i].Cells[0].Value = "$" + i.ToString();
+                MipsRegGrid.Rows[i].Cells[1].Value = reg[i].ToString();
+
+            }
+            MemGrid.Rows.Add(256);
+
+            for (int i = 0; i < 256; i++)
+            {
+                MemGrid.Rows[i].Cells[0].Value = i.ToString();
+                MemGrid.Rows[i].Cells[1].Value = "99";
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PiplGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
